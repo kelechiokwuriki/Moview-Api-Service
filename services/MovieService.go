b07be723b06af849f037b32556e3d4484d2d60fd
Kelechi Okwuriki <kelechiokwuriki@Kelechis-MacBook-Pro.local>
2020-12-31 15:07:21 +0000

    implementing methods for movieservice interface

--- services/MovieService.go
+++ services/MovieService.go
@@ -1,9 +1,38 @@
 package services
 
-import "Moview/models"
+import (
+	"Moview/models"
+	"errors"
+)
 
 type MovieService interface {
 	GetAllMovies() ([]models.Movie, error)
 	CreateMovie(movie *models.Movie) (*models.Movie, error)
 	ValidateMovie(movie *models.Movie) error
+}
+
+type movieService struct{}
+
+func NewMovieService() MovieService {
+	return &movieService
+}
+
+func (*movieService) GetAllMovies() ([]models.Movie, error) {
+
+}
+
+func (*movieService) CreateMovie(movie *models.Movie) (*models.Movie, error) {
+
+}
+
+func (*movieService) ValidateMovie(movie *models.Movie) error {
+	if movie == nil {
+		return errors.New("Invalid movie")
+	}
+
+	if movie.Name == "" || movie.YearOfRelease == 0 || movie.Actors == nil {
+		return errors.New("Invalid movie, missing either name, year of release or actors")
+	}
+
+	return nil
 }
